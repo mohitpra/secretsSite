2a260b808e26d4711e36e829ddb3b5e8c04b2ac9
Mohit <ymohit2142@gmail.com>
2021-06-15 09:13:20 +0530

    Add mongoDB Atlas as database

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 mongoose.set("useCreateIndex", true);
 
-mongoose.connect("mongodb://localhost:27017/userDB", {
+mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
